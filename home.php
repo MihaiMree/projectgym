32cfc75923a980720ff5befc6daf1d418fe7314d
MihaiMree <122447324+MihaiMree@users.noreply.github.com>
2023-08-30 15:03:56 +0200

    meer dingen

--- home.php
+++ home.php
@@ -15,10 +15,17 @@
 <div class="image">
        <img class="logo" src="assets/img/prog.png">
 </div>
-<div class="d-flex flex-column flex-shrink-0 bg-black" style="width: 7.1rem;height: 100vh">
+<h1 class="main">
+    Choose your muscule group
+</h1>
+<div class="chest">
+
+</div>
+
+<div class="d-flex flex-column flex-shrink-0 bg-black side-bar" >
     <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
 
-        <li class="nav-item">
+        <li class="nav-item ">
             <a href="home.php" class="nav-link active py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Home" data-bs-original-title="Home">
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
                 <div class="mask flex-center">
@@ -52,7 +59,7 @@
         </li
     </ul>
 </div>
-</div>
+
 
 </body>
 </html>
