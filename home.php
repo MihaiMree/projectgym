31341d407e99364cd94ad5b945cfc10330815336
MihaiMree <122447324+MihaiMree@users.noreply.github.com>
2023-09-01 09:29:41 +0200

    pics prequel

--- home.php
+++ home.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
     <title>Gym tracking</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
@@ -82,7 +82,7 @@
     </div>
     <div class="col">
         <div class="card shadow-sm">
-            <img src="/bootstrap/images/flower.jpg" alt="Bootstrap"  height="100" class="border border-white" preserveaspectratio="xMidYMid slice" focusable="false">
+            <img src="assets/img/legs.webp" alt="Bootstrap" width="280vw" height="100%" class="group"  focusable="false">
             <title>Placeholder</title>
             <rect width="50" height="100%" fill="#55595c"><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text>
 
@@ -179,6 +179,8 @@
 
 </div>
 
+
+<script src="assets/js/script.js"></script>
 </body>
 </html>
 
